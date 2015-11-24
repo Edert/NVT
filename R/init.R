79b1e566c59e9237bd1104178307f00a5ebba3aa
NexusX <NexusX@gmx.net>
2015-11-24 10:19:26 +0100

    1

--- R/init.R
+++ R/init.R
@@ -1,9 +1,20 @@
-
+#initialize and load input
 init <- function() {
 
 }
 
-
 load_expression <- function() {
+
+}
+
+check_hkgene_list <- function() {
+
+}
+
+check_expression_list <- function() {
+
+}
+
+check_method <- function() {
 
 }
